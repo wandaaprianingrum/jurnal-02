943c5bf8a58c27c32f1d42492850fed986a96782
wandaaprianingrum <wandaningrum1@gmail.com>
2020-09-20 13:57:25 +0700

    hapus file HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-	public static void main(String[] args){
-		System.out.println("Nama Lengkap : Wanda Aprianingrum");
-		System.out.println("Nama Panggilan : Wanda");
-		System.out.println("Kelas : D3IF_44_01");
-		System.out.println("NIM : 6706201027");
-	}
-}
